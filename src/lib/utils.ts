88b3fe559c6c25ef954e87b515bb210fa43667ab
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-06 06:36:13 +0700

    🦈 Update utils
    Add toRp and calculateTotal functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { CartItem } from "~/app/order/_hooks/useCart";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function toRp(amount: number): string {
+  return `Rp. ${amount.toLocaleString("id-ID")}`;
+}
+
+export function calculateTotal(items: CartItem[]) {
+  return items.reduce(
+    (total, { product }) => total + product.price * product.amount,
+    0,
+  );
 }
