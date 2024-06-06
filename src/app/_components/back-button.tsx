8169286fcd10ba167464e010caf08a885eda02f3
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-06 21:05:31 +0700

    🦙 Create back button

---
+++ src/app/_components/back-button.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
+
+export default function BackButton() {
+  const router = useRouter();
+
+  return (
+    <Button
+      variant={"outline"}
+      size={"default"}
+      className="gap-1 self-end"
+      onClick={() => router.back()}
+    >
+      <ArrowLeft className="h-4 w-4" />
+      <span>Kembali</span>
+    </Button>
+  );
+}
