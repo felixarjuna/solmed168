a2f33ca3f2a9c42048d20a235703a30349f0572f
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-06 15:39:14 +0700

    🦬 Refactor add order button
    - Handle loading, error and success

--- src/app/_components/add-order-button.tsx
+++ src/app/_components/add-order-button.tsx
@@ -1,36 +1,55 @@
 "use client";
 
-import { ShoppingCart } from "lucide-react";
+import { Loader2, ShoppingCart } from "lucide-react";
 import { Button } from "~/components/ui/button";
-import addOrder from "../order/_actions/order-actions";
 import { NewOrder } from "~/server/db/schema";
 import { useToast } from "~/components/ui/use-toast";
+import { useAction } from "next-safe-action/hooks";
+import { safeAddOrder } from "../order/_actions/order-actions";
+import { useRouter } from "next/navigation";
+import { useCart } from "../order/_hooks/useCart";
 
 interface AddOrderButtonProps {
   readonly order: NewOrder;
 }
 
 export default function AddOrderButton({ order }: AddOrderButtonProps) {
+  const router = useRouter();
   const { toast } = useToast();
+  const { clearCart } = useCart();
+
+  const { execute, status } = useAction(safeAddOrder, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        /** show toast for success scenario, clear cart and re-route to home page */
+        toast({
+          title: "Pesanan berhasil. ✅",
+          description: "Pesanan anda telah berhasil ditambahkan.",
+        });
+        clearCart();
+        router.push("/");
+      } else
+        toast({
+          title: "Pesanan gagal. ❌",
+          description: "Pesanan anda gagal ditambahkan. ",
+        });
+    },
+  });
+
   return (
     <Button
       className="flex w-fit items-center gap-2"
-      onClick={async () => {
-        const res = await addOrder(order);
-        if (res == "successfull")
-          toast({
-            title: "Pesanan berhasil. ✅",
-            description: "Pesanan anda telah berhasil ditambahkan.",
-          });
-        else
-          toast({
-            title: "Pesanan gagal. ❌",
-            description: "Pesanan anda gagal ditambahkan. ",
-          });
-      }}
+      onClick={() => execute(order)}
+      disabled={status === "executing"}
     >
-      <ShoppingCart className="h-4 w-4" />
-      <p>Selesai</p>
+      <div className="flex items-center gap-2">
+        {status === "executing" ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <ShoppingCart className="h-4 w-4" />
+        )}
+        <p>Selesai</p>
+      </div>
     </Button>
   );
 }
