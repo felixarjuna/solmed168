217f66dca1510239f1c4c8969778cfc888be2a71
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-06 22:09:45 +0700

    🦭 Add path revalidation

--- src/app/order/_actions/order-actions.ts
+++ src/app/order/_actions/order-actions.ts
@@ -4,8 +4,10 @@ import { db } from "~/server/db";
 import { orders } from "~/server/db/schema";
 import { action } from "./root";
 import { AddOrderValidator } from "../_validators/order";
+import { revalidatePath } from "next/cache";
 
 export const safeAddOrder = action(AddOrderValidator, async (order) => {
   await db.insert(orders).values(order);
+  revalidatePath("/order-history");
   return { success: true };
 });
