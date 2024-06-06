848202e57b53babe729fefbf744ebf649660cecd
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-06 07:13:45 +0700

    🐫 Add number of items

--- src/app/_components/order-summary.tsx
+++ src/app/_components/order-summary.tsx
@@ -3,16 +3,25 @@
 import { ShoppingBag } from "lucide-react";
 import CartButton from "./cart-button";
 import { useCart } from "../order/_hooks/useCart";
-import { calculateTotal, toRp } from "~/lib/utils";
-import Cart, { CartTriggerText } from "./cart";
+import { toRp } from "~/lib/utils";
+import { CartTriggerText } from "./cart";
 
 export default function OrderSummary() {
   const { items, cartTotal } = useCart();
+  const numberOfItems = items.reduce(
+    (total, { product }) => total + product.amount,
+    0,
+  );
 
   return (
     <section className="F fixed bottom-0 flex w-full items-center justify-between rounded-lg bg-neutral-100 p-4">
-      <div className="flex items-center gap-2 text-sm">
-        <ShoppingBag />
+      <div className="flex items-center gap-3 text-sm">
+        <div className="relative">
+          <ShoppingBag />
+          <span className="absolute -top-2 left-3 flex h-5 w-5 items-center justify-center rounded-full border bg-neutral-100/90 text-[0.7rem]">
+            {numberOfItems}
+          </span>
+        </div>
         <div>
           <p>
             Total: <span className="font-bold">{toRp(cartTotal)}</span>
