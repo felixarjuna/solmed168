0ea5ccb5d350243daa1dce76fe1bb0dd04a48088
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-10 11:19:08 +0700

    🐅 Update order page

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,33 +1,58 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import MenuCard from "../_components/menu-card";
 import { useCart } from "./_hooks/useCart";
 import { Button } from "~/components/ui/button";
-import { ArrowLeft, ShoppingBag, Utensils } from "lucide-react";
+import { Printer, ShoppingBag, Utensils } from "lucide-react";
 import AddOrderButton from "../_components/add-order-button";
 import { NewOrder } from "~/server/db/schema";
 import { toRp, today } from "~/lib/utils";
 import { Separator } from "~/components/ui/separator";
 import BackButton from "../_components/back-button";
+import Invoice, { InvoiceContent } from "../_components/invoice";
+import { useReactToPrint } from "react-to-print";
+import React from "react";
 
 export default function Page() {
-  const router = useRouter();
-
   const { items, cartTotal } = useCart();
   const numberOfItems = items.reduce(
     (total, { product }) => total + product.amount,
     0,
   );
 
+  /** Add new order. Table ID is now default to 1. */
   const order: NewOrder = {
     tableId: 1,
     products: items,
     totalAmount: cartTotal,
   };
 
+  /** Define component to be referenced for invoice printing */
+  const [isPrinting, setIsPrinting] = React.useState<boolean>(false);
+  const printRef = React.useRef(null);
+  const promiseRef = React.useRef<((value?: any) => void) | null>(null);
+
+  React.useEffect(() => {
+    if (isPrinting && promiseRef.current) {
+      // Resolves the Promise, letting `react-to-print` know that the DOM updates are completed
+      promiseRef.current();
+    }
+  }, [isPrinting]);
+
+  const onPrint = useReactToPrint({
+    onBeforeGetContent: () => {
+      return new Promise((resolve) => {
+        promiseRef.current = resolve;
+        setIsPrinting(true);
+      });
+    },
+    onAfterPrint: () => {
+      promiseRef.current = null;
+      setIsPrinting(false);
+    },
+  });
+
   return (
-    <main>
+    <main className="z-0">
       <section className="m-4 flex flex-col gap-4 p-4">
         <BackButton />
 
@@ -45,15 +70,10 @@ export default function Page() {
 
         <div className="flex flex-col gap-2">
           <h3 className="font-bold">Rincian order</h3>
-          <div className="mb-20 flex flex-col gap-2">
-            {items.map((item) => (
-              <MenuCard
-                key={item.product.id}
-                menu={item.product}
-                amount={item.product.amount}
-                isAdjustable={false}
-              />
-            ))}
+          {<InvoiceContent items={items} totalAmount={cartTotal} />}
+
+          <div ref={printRef} className={isPrinting ? "mt-4 p-8" : "hidden"}>
+            <Invoice items={items} totalAmount={cartTotal} />
           </div>
         </div>
       </section>
@@ -68,7 +88,15 @@ export default function Page() {
             Total: <span className="font-bold">{toRp(cartTotal)}</span>
           </p>
         </div>
-        <AddOrderButton order={order} />
+        <div className="flex gap-2">
+          <Button
+            size={"icon"}
+            onClick={() => onPrint(null, () => printRef.current)}
+          >
+            <Printer className="h-4 w-4" />
+          </Button>
+          <AddOrderButton order={order} />
+        </div>
       </section>
     </main>
   );
