0df3ec004a777c79956e7e650cef16df0b748f3b
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-27 22:18:23 +0200

    🐏 Update payment method button
    - Handle change payment method
    - Handle payment logic

--- src/app/_components/payment-method-button.tsx
+++ src/app/_components/payment-method-button.tsx
@@ -1,9 +1,19 @@
 "use client";
 
 import * as _ from "lodash";
-import { Banknote, Landmark, Loader2, QrCode, X } from "lucide-react";
+import {
+  ArrowLeft,
+  Banknote,
+  Landmark,
+  Loader2,
+  PartyPopper,
+  QrCode,
+  WalletMinimal,
+  X,
+} from "lucide-react";
 import { useAction } from "next-safe-action/hooks";
 import { useRouter } from "next/navigation";
+import React from "react";
 import { Button, buttonVariants } from "~/components/ui/button";
 import {
   Drawer,
@@ -16,36 +26,34 @@ import {
   DrawerTrigger,
 } from "~/components/ui/drawer";
 import { useToast } from "~/components/ui/use-toast";
-import { cn } from "~/lib/utils";
-import { type NewOrder } from "~/server/db/schema";
-import { paymentMethods, type PaymentMethodType } from "../menu";
-import { safeAddOrder } from "../order/_actions/order-actions";
+import { cn, toRp } from "~/lib/utils";
+import { type Order } from "~/server/db/schema";
+import { paymentMethods, type PaymentMethodType } from "../data";
+import { safePayOrder } from "../order/_actions/order-actions";
 import { useCart } from "../order/_hooks/useCart";
+import { useThermalPrinter } from "../order/_hooks/useThermalPrinter";
 
 interface PayButtonProps {
-  readonly order: NewOrder;
-  readonly onPrintInvoice: () => void;
+  readonly order: Order;
 }
 
-export default function PayButton({ order, onPrintInvoice }: PayButtonProps) {
+export default function PayButton({ order }: PayButtonProps) {
   const router = useRouter();
   const { toast } = useToast();
   const { clearCart } = useCart();
 
-  const { execute, status } = useAction(safeAddOrder, {
+  const { execute, status } = useAction(safePayOrder, {
     onSuccess: ({ success }) => {
       if (success) {
         /** show toast for success scenario, clear cart and re-route to home page */
         toast({
-          title: "Pesanan berhasil. ✅",
-          description: "Pesanan anda telah berhasil ditambahkan.",
+          title: "Pembayaran berhasil. ✅",
         });
         clearCart();
         router.push("/");
       } else
         toast({
-          title: "Pesanan gagal. ❌",
-          description: "Pesanan anda gagal ditambahkan. ",
+          title: "Pembayaran gagal. ❌",
         });
     },
   });
@@ -56,26 +64,38 @@ export default function PayButton({ order, onPrintInvoice }: PayButtonProps) {
     if (method === "transfer") return <Landmark />;
   };
 
+  const { onPrint } = useThermalPrinter(order.products);
+
+  const [paymentMethod, setPaymentMethod] = React.useState<PaymentMethodType>();
+  const onSelectPaymentMethod = (method?: PaymentMethodType) => {
+    setPaymentMethod(method);
+  };
+
   /** Method to handle payment: cash, qris, and transfer.
    * The current workflow must be executed for each transaction:
    * 1. handle payment
    * 2. save order to database if payment success
    * 3. print order invoice
    */
-  const onClickPaymentMethod = (method: PaymentMethodType) => {
+  const onPaymentDone = async (method: PaymentMethodType) => {
     const _order = { ...order, paymentMethod: method };
     execute(_order);
-    onPrintInvoice();
+    await onPrint();
   };
 
   return (
     <Drawer>
-      <DrawerTrigger className={cn(buttonVariants({ variant: "default" }))}>
+      <DrawerTrigger
+        className={cn(
+          buttonVariants({ variant: "default", size: "sm" }),
+          "animate-pulse",
+        )}
+      >
         <div className="flex h-4 items-center gap-2">
           {status === "executing" ? (
             <Loader2 className="h-4 w-4 animate-spin" />
           ) : (
-            <Banknote className="h-4 w-4" />
+            <WalletMinimal className="h-4 w-4" />
           )}
           <p>Bayar</p>
         </div>
@@ -87,19 +107,77 @@ export default function PayButton({ order, onPrintInvoice }: PayButtonProps) {
             Silahkan pilih metode pembayaran di bawah ini.
           </DrawerDescription>
         </DrawerHeader>
-        <div className="flex flex-col gap-2">
-          {paymentMethods.map((method, i) => (
-            <Button
-              key={i}
-              className="flex justify-start gap-2 rounded-lg border px-6 py-4"
-              size={"lg"}
-              onClick={() => onClickPaymentMethod(method)}
-            >
-              {getIconFromPaymentMethod(method)}
-              <p>{_.upperCase(method)}</p>
-            </Button>
-          ))}
-        </div>
+
+        {paymentMethod === undefined ? (
+          <div className="flex flex-col gap-2">
+            {paymentMethods.map((method, i) => (
+              <Button
+                key={i}
+                className="flex justify-start gap-2 rounded-lg border px-6 py-4"
+                size={"lg"}
+                onClick={() => setPaymentMethod(method)}
+              >
+                {getIconFromPaymentMethod(method)}
+                <p>{_.upperCase(method)}</p>
+              </Button>
+            ))}
+          </div>
+        ) : null}
+
+        {paymentMethod === "cash" ? (
+          <div className="grid min-w-60 gap-8">
+            <div className="text-center">
+              <p>Nominal pembayaran </p>
+              <p className="text-3xl font-bold">{toRp(order.totalAmount)}</p>
+            </div>
+
+            <ActionButton
+              method={paymentMethod}
+              onSelectPaymentMethod={onSelectPaymentMethod}
+              onPaymentDone={onPaymentDone}
+            />
+          </div>
+        ) : null}
+
+        {paymentMethod === "qris" ? (
+          <div className="grid min-w-60 gap-8">
+            <div className="text-center">
+              <p className="text-3xl font-bold">QRIS</p>
+              <p className="text-3xl font-bold">{toRp(order.totalAmount)}</p>
+            </div>
+
+            <ActionButton
+              method={paymentMethod}
+              onSelectPaymentMethod={onSelectPaymentMethod}
+              onPaymentDone={onPaymentDone}
+            />
+          </div>
+        ) : null}
+
+        {paymentMethod === "transfer" ? (
+          <div className="grid min-w-60 gap-4">
+            <div className="text-center">
+              <p>Nominal pembayaran</p>
+              <p className="text-3xl font-bold">{toRp(order.totalAmount)}</p>
+            </div>
+
+            <div className="flex gap-2 text-center">
+              <div>
+                <p>No. Rekening pembayaran</p>
+                <p className="text-3xl font-bold">
+                  <span className="italic text-blue-700">BCA</span> - 7881031331
+                </p>
+              </div>
+            </div>
+
+            <ActionButton
+              method={paymentMethod}
+              onSelectPaymentMethod={onSelectPaymentMethod}
+              onPaymentDone={onPaymentDone}
+            />
+          </div>
+        ) : null}
+
         <DrawerFooter>
           <DrawerClose
             className={buttonVariants({ variant: "outline", size: "icon" })}
@@ -109,5 +187,43 @@ export default function PayButton({ order, onPrintInvoice }: PayButtonProps) {
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
+  );
+}
+
+interface IActionButton {
+  readonly method: PaymentMethodType;
+  readonly onSelectPaymentMethod: (
+    method: PaymentMethodType | undefined,
+  ) => void;
+  readonly onPaymentDone: (method: PaymentMethodType) => Promise<void>;
+}
+
+function ActionButton({
+  method,
+  onSelectPaymentMethod,
+  onPaymentDone,
+}: IActionButton) {
+  return (
+    <div className="mt-8 flex items-center justify-between">
+      <Button
+        size={"sm"}
+        variant={"secondary"}
+        className="flex w-24 items-center gap-1"
+        onClick={() => onSelectPaymentMethod(undefined)}
+      >
+        <ArrowLeft className="h-4 w-4" />
+        <p>Kembali</p>
+      </Button>
+
+      <Button
+        size={"sm"}
+        variant={"default"}
+        className="flex w-24 items-center gap-2 font-normal"
+        onClick={() => onPaymentDone(method)}
+      >
+        <PartyPopper className="h-4 w-4" />
+        <p>Selesai</p>
+      </Button>
+    </div>
   );
 }
