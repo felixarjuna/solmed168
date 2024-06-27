44b6f35a36c79fa5a5216cdd3a183c90f6f17f85
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-27 18:56:31 +0200

    🐷 Create active order button component

---
+++ src/app/_components/active-order-button.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { ScrollText } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
+
+export default function ActiveOrderButton() {
+  const router = useRouter();
+
+  return (
+    <Button
+      variant={"outline"}
+      size={"sm"}
+      className="relative flex items-center gap-1"
+      onClick={() => router.push("/order-history?active=true")}
+    >
+      <ScrollText className="h-4 w-4" />
+      <span className="absolute -right-1 -top-1 flex h-3 w-3">
+        <span className="absolute inline-flex h-full w-full  animate-ping rounded-full bg-cyan-400 opacity-75" />
+        <span className="relative inline-flex h-3 w-3 rounded-full bg-cyan-400"></span>
+      </span>
+      <p>Active Order</p>
+    </Button>
+  );
+}
