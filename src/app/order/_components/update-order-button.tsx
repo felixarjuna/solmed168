05c8facc437268e6c412af27d490140003efcd12
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-06-28 10:09:16 +0200

    🐷 Create update order button

---
+++ src/app/order/_components/update-order-button.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { Loader2, PlusCircle } from "lucide-react";
+import { useAction } from "next-safe-action/hooks";
+import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
+import { useToast } from "~/components/ui/use-toast";
+import { calculateTotal } from "~/lib/utils";
+import { safeUpdateOrder } from "../_actions/order-actions";
+import { useCart, type CartItem } from "../_hooks/useCart";
+
+interface IUpdateOrderButtonProps {
+  readonly orderId: number;
+  readonly products: CartItem[];
+}
+
+export default function UpdateOrderButton({
+  orderId,
+  products,
+}: IUpdateOrderButtonProps) {
+  const router = useRouter();
+  const { toast } = useToast();
+  const { clearCart } = useCart();
+
+  const { execute, status } = useAction(safeUpdateOrder, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        /** show toast for success scenario, clear cart and re-route to home page */
+        toast({
+          title: "Update pesanan berhasil. ✅",
+          description: "Pesanan anda telah berhasil diupdate.",
+        });
+        clearCart();
+        router.push("/order-history?active=true");
+      } else
+        toast({
+          title: "Update pesanan gagal. ❌",
+          description: "Pesanan anda gagal diupdate. ",
+        });
+    },
+    onError: () => {
+      toast({
+        title: "Update pesanan gagal. ❌",
+        description: "Pesanan anda gagal diupdate. ",
+      });
+    },
+  });
+
+  const onUpdateOrder = async () => {
+    execute({
+      orderId: orderId,
+      products: products,
+      totalAmount: calculateTotal(products),
+    });
+  };
+
+  return (
+    <Button onClick={onUpdateOrder}>
+      <div className="flex h-4 items-center gap-2">
+        {status === "executing" ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <PlusCircle className="h-4 w-4" />
+        )}
+        <p>Pesan</p>
+      </div>
+    </Button>
+  );
+}
