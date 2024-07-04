59a537c6508ead63b99e5726cd35a69756c0be61
Felix Arjuna <felixarjuna.de@yahoo.com>
2024-07-04 19:05:29 +0200

    Update logo.tsx

--- src/app/_components/logo.tsx
+++ src/app/_components/logo.tsx
@@ -11,7 +11,7 @@ export default function Logo() {
       className="m-8 mb-0 flex items-center gap-1"
       onClick={() => router.push("/")}
     >
-      <Image src={logo} alt="solmed-logo" width={50} height={50} />
+      <Image src={logo} alt="solmed-logo" width={70} height={70} />
       <h1 className="text-3xl font-bold">SOLMED 168</h1>
     </nav>
   );
